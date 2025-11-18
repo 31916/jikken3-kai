724fdd0f21dd88f26a53cdd093fe5fe00f553b40
hb231232-ui <hb231232@g.u-fukui.ac.jp>
2025-11-18 15:46:27 +0900

    上へ

--- static/humbergur.js
+++ static/humbergur.js
@@ -6,3 +6,26 @@ menuIcon.addEventListener("click", () => {
     sidebar.classList.toggle("active");
     menuIcon.classList.toggle("active");
 });
+
+const backToTopButton = document.getElementById("back-to-top");
+
+// 初期状態ではボタンを非表示にする
+backToTopButton.style.display = "none"; 
+
+// ページをスクロールしたときの動作
+window.addEventListener("scroll", () => {
+    // スクロール量が200pxを超えたらボタンを表示
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+});
+
+// ボタンをクリックしたときの動作
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // スムーズスクロールを有効にする
+    });
+});
